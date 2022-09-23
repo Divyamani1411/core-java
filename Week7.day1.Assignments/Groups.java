c18be6dc9908a41a4098379654c05808e64ac2da
Divyamani <110727893+Divyamani1411@users.noreply.github.com>
2022-09-23 21:57:52 +0530

    Week7.Day1.Assignments
    Week7.Day1.Assignments

---
+++ Week7.day1.Assignments/Groups.java
@@ -0,0 +1,126 @@
+package week7.day1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Groups extends LeafTapBases {
+	@Test(groups="regression")
+	public void createLead() {
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TL");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("divya");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Mani");
+		driver.findElement(By.name("submitButton")).click();
+	}
+
+	@Test(dependsOnMethods = "createLead",groups="regression")
+	public void editLead() throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
+		driver.findElement(By.name("submitButton")).click();
+	}
+
+	@Test(groups="smoke")
+	public void deleteLead() throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("9");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		String leadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).getText();
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Delete")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//input[@name='id']")).sendKeys(leadID);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		String text = driver.findElement(By.className("x-paging-info")).getText();
+		if (text.equals("No records to display")) {
+			System.out.println("Text matched");
+		} else {
+			System.out.println("Text not matched");
+		}
+	}
+
+	@Test(groups="smoke")
+	public void duplicateLead() throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.name("submitButton")).click();
+	}
+
+	@Test(groups="functional")
+	public void mergeLead() throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Merge Leads")).click();
+		driver.findElement(By.xpath("//img[@alt='Lookup']")).click();
+		Set<String> allWindows = driver.getWindowHandles();
+		List<String> allhandles = new ArrayList<String>(allWindows);
+		driver.switchTo().window(allhandles.get(1));
+		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("hari");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(1000);
+		String leadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).getText();
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.switchTo().window(allhandles.get(0));
+		driver.findElement(By.xpath("(//img[@alt='Lookup'])[2]")).click();
+		Set<String> allWindows2 = driver.getWindowHandles();
+		List<String> allhandles2 = new ArrayList<String>(allWindows2);
+		driver.switchTo().window(allhandles2.get(1));
+		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("babu");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.switchTo().window(allhandles2.get(0));
+		driver.findElement(By.xpath("//a[text()='Merge']")).click();
+		driver.switchTo().alert().accept();	
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//input[@name='id']")).sendKeys(leadID);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		String text = driver.findElement(By.className("x-paging-info")).getText();
+		if (text.equals("No records to display")) {
+			System.out.println("Text matched");
+		} else {
+			System.out.println("Text not matched");
+		}
+		
+	}
+
+}
